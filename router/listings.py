b56a7776dc82581f682dd31d1e987a919f572e5e
v3lkr0w <r3v3rm@protonmail.com>
2021-03-13 05:20:16 -0500

    adding database to listing

--- router/listings.py
+++ router/listings.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends, Form
+from fastapi import APIRouter, status, Depends, Form, status, HTTPException
 from .auth import manager
 import database, models
 
@@ -9,8 +9,15 @@ router = APIRouter(
 )
 
 @router.post('/create', status_code=status.HTTP_200_OK, response_model=models.Listing)
-def protected(title: str = Form(...), context: str = Form(...), user = Depends(manager), db:database.Session = Depends(database.get_db)):
-    
-    
+def create_listing(title: str = Form(...), context: str = Form(...), user = Depends(manager), db:database.Session = Depends(database.get_db)):
+    if len(title) >= 100:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Title too long')
+    elif len(context) >= 1000:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail='Context too long')
+    else:
+        db_listing = database.Listing(title=title, context=context)
+        db.add(db_listing)
+        db.commit()
+        db.refresh(db_listing)
     
     return models.Listing(title=title, context=context)
